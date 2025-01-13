02ea9388ec088ed8dd59b3564e17e49cfa3a2ce0
Cristian-Maxi <cristiangomez0686@outlook.com>
2025-01-12 23:11:28 -0300

    Spring 3 Finalizado

---
+++ src/main/java/com/mindhub/todolist/controllers/AutenticacionController.java
@@ -0,0 +1,24 @@
+package com.mindhub.todolist.controllers;
+
+import com.mindhub.todolist.dtos.TokenDTO.JWTTokenDTO;
+import com.mindhub.todolist.dtos.UserEntityDTO.DatosAutenticacionUsuario;
+import com.mindhub.todolist.services.IAutenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/login")
+public class AutenticacionController {
+
+    @Autowired
+    private IAutenticationService autenticacionService;
+
+    @PostMapping
+    public ResponseEntity<JWTTokenDTO> autenticar(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
+        JWTTokenDTO jwtTokenDTO = autenticacionService.autenticar(datosAutenticacionUsuario);
+        return ResponseEntity.ok(jwtTokenDTO);
+    }
+
+}
